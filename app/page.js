2565afade7cd3caff9f669ac705a6276e9b6fcda
sf001-ic <shafieyan.2001@gmail.com>
2026-05-30 09:31:00 +0330

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif", direction: "rtl" }}>
+      <h1>سلام! این سایت من روی Vercel است</h1>
+      <p>این پروژه مستقیم با GitHub ساخته شده و روی Vercel deploy شده.</p>
+      <a href="/about">برو به صفحه درباره من</a>
+    </main>
+  );
+}
