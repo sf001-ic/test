59ffa852c0ef1b0a392385c5b553f896183dfb4d
sf001-ic <shafieyan.2001@gmail.com>
2026-05-30 09:31:29 +0330

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif", direction: "rtl" }}>
+      <h1>درباره من</h1>
+      <p>این صفحه درباره من است. خیلی شیک، خیلی ساده، بدون ادا و اصول اضافه 😎</p>
+      <a href="/">بازگشت</a>
+    </main>
+  );
+}
